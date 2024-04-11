1a75f5382d38c42f5c82e122a5afc51883f45353
Musa Tanrıverdi <musatanriiverdii@gmail.com>
2024-04-11 20:48:00 +0300

    Implement basic Todo App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,33 @@ import { useState } from 'react'
 
 function App() {
 
+  const [Todos, setTodos] = useState(["musa", "emin"])
+  const [inputValue, setInputValue] = useState('')
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='w-full min-h-screen flex items-center justify-center flex-col'>
+      <h1 className='flex'>Todos</h1>
+      {Todos.map((e, i) => {
+        return (
+          <div key={i}>
+            <p className=''>{e}</p>
+            <button onClick={() => {
+              setTodos([
+                ...Todos.slice(0, i),
+                ...Todos.slice(i + 1)])
+            }} className='bg-red-500'>Delete</button>
+          </div>
+        )
+      })
+      }
+      <div>
+        <input value={inputValue} onChange={e => setInputValue(e.target.value)} type="text" placeholder='Add Todo' />
+        <button onClick={() => {
+          setTodos([...Todos, inputValue])
+          setInputValue('')
+        }} className='bg-green-500 border-solid'>Add</button>
+      </div>
+    </div>
   )
 }
 
