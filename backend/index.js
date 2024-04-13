d0676d896543950c0d2e6df92f722394f44c8352
Musa Tanrıverdi <musatanriiverdii@gmail.com>
2024-04-13 16:11:02 +0300

    connect to database and fetch data

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import express from "express"
+import connectMongoDb from "./mongoose.js"
+import todoModel from './models/todo.js'
 
 const app = express()
+connectMongoDb()
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', async function (req, res) {
+    const todos = await todoModel.find()
+    res.send(todos)
 })
 
 app.listen(3000)
