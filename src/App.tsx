ee768136c40ed377250eb838bb504a395efa49ed
Musa Tanrıverdi <musatanriiverdii@gmail.com>
2024-04-13 15:11:12 +0300

    Fix conflict error

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,6 @@ function App() {
 
 
   return (
-<<<<<<< Updated upstream
-    <div className='w-full min-h-screen flex items-center justify-center flex-col'>
-      <h1 className='flex'>Todos</h1>
-      {Todos.map((e, i) => {
-        return (
-          <div key={i}>
-            <p className=''>{e}</p>
-            <button onClick={() => {
-              setTodos([
-                ...Todos.slice(0, i),
-                ...Todos.slice(i + 1)])
-            }} className='bg-red-500'>Delete</button>
-          </div>
-        )
-      })
-      }
-      <div>
-        <input value={inputValue} onChange={e => setInputValue(e.target.value)} type="text" placeholder='Add Todo' />
-        <button onClick={() => {
-          setTodos([...Todos, inputValue])
-          setInputValue('')
-        }} className='bg-green-500 border-solid hover:bg-green-300 transition'>Add</button>
-=======
     <div className='w-full min-h-screen flex items-center justify-center flex-col bg-gray-900'>
       <h2 className='flex mb-2 bg-gray-700 h-[50px] w-[500px] text-white items-center justify-center border border-gray-500 rounded-md font-bold'>Todos</h2>
 
@@ -55,7 +32,6 @@ function App() {
             setInputValue('')
           }} className='bg-blue-500 border-solid hover:bg-blue-400 transition w-[80px] rounded-md p-2 ml-2 text-white'>Add</button>
         </div>
->>>>>>> Stashed changes
       </div>
     </div>
   )
