7fbb4bc9742bcc8a3359f2ccfe17c8603bbe28f9
Musa Tanrıverdi <musatanriiverdii@gmail.com>
2024-04-14 21:16:04 +0300

    create post and delete function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
 
   const [Todos, setTodos] = useState(["musa", "emin"])
   const [inputValue, setInputValue] = useState('')
 
+  const handleAdd = () => {
+    setTodos([...Todos, inputValue])
+    setInputValue('')
+    axios.post('http://localhost:3000', { description: inputValue })
+  }
 
+  const handleDelete = (id: any) => {
+    setTodos([...Todos.slice(0, id), ...Todos.slice(id + 1)])
+    axios.delete('http://localhost:3000/' + id)
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:3000')
+      .then(result => console.log(result.data))
+  }, [])
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center flex-col bg-gray-900'>
@@ -16,24 +31,17 @@ function App() {
           return (
             <div className='bg-gray-600 border border-gray-500 rounded-md flex-row p-2 flex items-center justify-between text-white mb-4' key={i}>
               <p className=''>{e}</p>
-              <button onClick={() => {
-                setTodos([
-                  ...Todos.slice(0, i),
-                  ...Todos.slice(i + 1)])
-              }} className='bg-red-500 border-solid hover:bg-red-400 transition w-[80px] rounded-md p-2 ml-2 text-white'>Delete</button>
+              <button onClick={() => handleDelete(i)} className='bg-red-500 border-solid hover:bg-red-400 transition w-[80px] rounded-md p-2 ml-2 text-white'>Delete</button>
             </div>
           )
         })
         }
         <div className='flex items-center justify-between'>
           <input required className='outline-none bg-transparent border border-gray-500 p-2 w-[350px] text-white' value={inputValue} onChange={e => setInputValue(e.target.value)} type="text" placeholder='Add Todo' />
-          <button onClick={() => {
-            setTodos([...Todos, inputValue])
-            setInputValue('')
-          }} className='bg-blue-500 border-solid hover:bg-blue-400 transition w-[80px] rounded-md p-2 ml-2 text-white'>Add</button>
+          <button onClick={handleAdd} className='bg-blue-500 border-solid hover:bg-blue-400 transition w-[80px] rounded-md p-2 ml-2 text-white' > Add</button>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
